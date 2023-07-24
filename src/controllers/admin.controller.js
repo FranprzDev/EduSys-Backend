0d1b1673258c2a4294f1244f1bb3139da9aa324f
FranprzDev <franutn23@gmail.com>
2023-07-24 02:17:52 -0300

    Creación de los controladores de administración

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,132 @@
+const Admin = require("../models/admin.model");
+
+const createAdmin = async (req, res) => {
+  // Realizo la destructuración de la req
+  const { nombre, apellido, direccion, dni, celular, mail } = req.body;
+
+  // Genero mediante el modelo el Administrador
+
+  const adminGenerado = new Admin({
+    nombre: nombre,
+    apellido: apellido,
+    direccion: direccion,
+    dni: dni,
+    celular: celular,
+    mail: mail,
+  });
+
+  // Espero que se guarde
+  await adminGenerado.save();
+
+  // Genero el código HTTP & Un mensaje de respueta
+  res
+    .status(201)
+    .json({ message: `Se creo el Administrador ${nombre} ${apellido}` });
+};
+
+const deleteAdmin = async (req, res) => {
+  const adminGenerado = await Admin.findById(req.params.id);
+
+  if (adminGenerado === null) {
+    res.status(404);
+    return res.json({ message: "Administrador no encontrado" });
+  }
+
+  const filters = { _id: req.params.id };
+  await adminGenerado.deleteOne(filters);
+
+  res.status(204);
+  res.json({
+    message: `El Administrador ${adminGenerado.nombre} ${adminGenerado.apellido} fue eliminado de la DB.`,
+  });
+};
+
+const findAllAdmin = async (req, res) => {
+  const adminGenerado = await Admin.find();
+  if (adminGenerado != null) {
+    res.status(200);
+    res.json(adminGenerado);
+  } else {
+    res.status(404);
+    res.json({ message: "No se pudo encontrar la tabla de Administradores." });
+  }
+};
+
+const findAdminById = async (req, res) => {
+  const adminGenerado = await Admin.findById(req.params.id);
+
+  if (adminGenerado === null) {
+    res.status(404);
+    return res.json({ message: "Administrador no encontrado" });
+  }
+
+  res.json({ message: "Se encontró el administrador", datos: adminGenerado });
+};
+
+const updateAdminByID_mail = async (req, res) => {
+  const adminGenerado = await Admin.findById(req.params.id);
+  if (adminGenerado === null) {
+    res.status(404);
+    return res.json({ message: "Administrador no encontrado" });
+  }
+
+  // Lógica para la actualización por ID -> Mail
+  await Admin.findByIdAndUpdate(req.params.id, {
+    mail: req.body.mail,
+  });
+
+  res.status(200);
+  res.json({
+    message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
+    datos: adminGenerado,
+  });
+  // Preguntar por que los datos que llegan al postman aún no están actualizados [como que van 1 atras]
+};
+
+const updateAdminByID_celular = async (req, res) => {
+  const adminGenerado = await Admin.findById(req.params.id);
+  if (adminGenerado === null) {
+    res.status(404);
+    return res.json({ message: "Administrador no encontrado" });
+  }
+
+  // Lógica para la actualización por ID -> Celular
+  await Admin.findByIdAndUpdate(req.params.id, {
+    celular: req.body.celular,
+  });
+
+  res.status(200);
+  res.json({
+    message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
+    datos: adminGenerado,
+  });
+};
+
+const updateAdminByID_direccion = async (req, res) => {
+  const adminGenerado = await Admin.findById(req.params.id);
+  if (adminGenerado === null) {
+    res.status(404);
+    return res.json({ message: "Administrador no encontrado" });
+  }
+
+  // Lógica para la actualización por ID -> Dirección
+  await Admin.findByIdAndUpdate(req.params.id, {
+    direccion: req.body.direccion,
+  });
+
+  res.status(200);
+  res.json({
+    message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
+    datos: adminGenerado,
+  });
+};
+
+module.exports = {
+  createAdmin,
+  deleteAdmin,
+  findAllAdmin,
+  findAdminById,
+  updateAdminByID_mail,
+  updateAdminByID_celular,
+  updateAdminByID_direccion,
+};
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,146 +1,47 @@
 const { Router } = require("express");
 const Admin = require("../models/admin.model");
+const {
+  createAdmin,
+  deleteAdmin,
+  findAllAdmin,
+  findAdminById,
+  updateAdminByID_mail,
+  updateAdminByID_celular,
+  updateAdminByID_direccion,
+} = require("../controllers/admin.controller")
+
 
 const adminRouter = Router();
 // Operaciones para el CRUD de ADministradores (Only - Super Administrador)
 
+/* --> Creación <-- */
+
 // Operación de Creación
-adminRouter.post("/create", async (req, res) => {
-  // Realizo la destructuración de la req
-  const {
-    nombre,
-    apellido,
-    direccion,
-    dni,
-    celular,
-    mail
-  } = req.body
+adminRouter.post("/create", createAdmin);
 
-  // Genero mediante el modelo el Administrador
+/* --> Borrado <-- */
 
-  const adminGenerado = new Admin({
-    nombre: nombre,
-    apellido: apellido,
-    direccion: direccion,
-    dni: dni,
-    celular: celular,
-    mail: mail
-  })
-
-  // Espero que se guarde
-  await adminGenerado.save()
-
-  // Genero el código HTTP & Un mensaje de respueta
-  res.status(201).json({ message: `Se creo el Administrador ${nombre} ${apellido}` });
-});
+// Operación de Borrado  por ID
+adminRouter.delete("/delete-by-id/:id", deleteAdmin);
 
 
-// Operación de Borrado físico por ID
-adminRouter.delete("/delete-by-id/:id", async (req, res) => {
-  const adminGenerado = await Admin.findById(req.params.id);
-
-  if (adminGenerado === null) {
-    res.status(404)
-    return res.json({ message: "Administrador no encontrado" })
-  }
-
-  const filters = { _id: req.params.id }
-  await adminGenerado.deleteOne(filters)
-
-  res.status(204)
-  res.json({ message: `El Administrador ${adminGenerado.nombre} ${adminGenerado.apellido} fue eliminado de la DB.`});
-});
-
-
-// --> Busqueda <--
+/* --> Busqueda <-- */
 
 // Operación de Busqueda Total
-adminRouter.get("/findall", async (req, res) => {
-  const adminGenerado = await Admin.find()
-  if(adminGenerado != null){
-    res.status(200)
-    res.json(adminGenerado)
-  }else{
-    res.status(404)
-    res.json({ message: "No se pudo encontrar la tabla de Administradores." })
-  }
-
-});
+adminRouter.get("/findall", findAllAdmin);
 
 // Operación de Busqueda por ID
-adminRouter.get("/find-by-id/:id", async (req, res) => {
-  const adminGenerado = await Admin.findById(req.params.id);
+adminRouter.get("/find-by-id/:id", findAdminById);
 
-  if (adminGenerado === null) {
-    res.status(404)
-    return res.json({ message: "Administrador no encontrado" })
-  }
-
-  res.json({ message: "Se encontró el administrador", datos: adminGenerado });
-});
-
-
-// --> Actualización <-- 
+/* --> Actualización <-- */
 
 // Operación de Actualización Mail por ID
-adminRouter.put("/update-mail-by-id/:id", async (req, res) => { 
-    const adminGenerado = await Admin.findById(req.params.id);
-    if (adminGenerado === null) {
-      res.status(404);
-      return res.json({ message: "Administrador no encontrado" });
-    }
-
-    // Lógica para la actualización por ID -> Mail
-    await Admin.findByIdAndUpdate(req.params.id, {
-      mail: req.body.mail,
-    });
-
-    res.status(200);
-    res.json({
-      message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
-      datos: adminGenerado,
-    });
-    // Preguntar por que los datos que llegan al postman aún no están actualizados [como que van 1 atras]
-});
+adminRouter.put("/update-mail-by-id/:id", updateAdminByID_mail);
 
 // Operación de Actualización Celular por ID
-adminRouter.put("/update-phone-by-id/:id", async (req, res) => { 
-  const adminGenerado = await Admin.findById(req.params.id);
-  if (adminGenerado === null) {
-    res.status(404);
-    return res.json({ message: "Administrador no encontrado" });
-  }
-
-  // Lógica para la actualización por ID -> Celular
-  await Admin.findByIdAndUpdate(req.params.id, {
-    celular: req.body.celular
-  });
-
-  res.status(200);
-  res.json({
-    message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
-    datos: adminGenerado,
-  });
-});
+adminRouter.put("/update-phone-by-id/:id", updateAdminByID_celular);
 
 // Operación de Actualización Dirección por ID
-adminRouter.put("/update-direction-by-id/:id", async (req, res) => { 
-  const adminGenerado = await Admin.findById(req.params.id);
-  if (adminGenerado === null) {
-    res.status(404);
-    return res.json({ message: "Administrador no encontrado" });
-  }
-
-  // Lógica para la actualización por ID -> Dirección
-  await Admin.findByIdAndUpdate(req.params.id, {
-    direccion: req.body.direccion
-  });
-
-  res.status(200);
-  res.json({
-    message: `Se encontro el administrador ${adminGenerado.nombre} ${adminGenerado.apellido} y se actualizaron sus datos [Mail]`,
-    datos: adminGenerado,
-  });
-});
+adminRouter.put("/update-direction-by-id/:id", updateAdminByID_direccion);
 
 module.exports = adminRouter;
