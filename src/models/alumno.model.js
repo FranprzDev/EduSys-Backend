6bd353d21ece47d757e5bd958f41a29ff356c1ba
Gise Delgado <giseladelgado909@gmail.com>
2023-07-28 20:37:03 -0300

    Creación del model alumno

---
+++ src/models/alumno.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+const alumnoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 25
+    },
+    apellido: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 25
+    },
+    añoCursado: {
+        type: Number ,
+        required: true,
+        trim: true, 
+        minLength: 1,
+        maxLength: 1,
+    },
+    alDia: {
+        type: Boolean,
+        required: true,
+        trim: true,
+        unique: false,
+    },
+  
+   
+  
+   
+    
+})
+
+const Alumno = mongoose.model("Alumno", alumnoSchema)
+
+module.exports = Alumno;
