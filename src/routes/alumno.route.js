25bc91df59f03ab637fd35cf8c5e084966199a4b
Gise Delgado <giseladelgado909@gmail.com>
2023-07-31 20:36:42 -0300

    Creacion de la ruta alumno

---
+++ src/routes/alumno.route.js
@@ -0,0 +1,71 @@
+const { Router } = require("express");
+const {
+  createAdmin,
+  deleteAdmin,
+  findAllAdmin,
+  findAdminById,
+
+} = require("../controllers/alumno.controller")
+const { body, param } = require("express-validator");
+const { expressValidations } = require("../middlewares/common.validations");
+// const { verifyToken } = require("../controllers/adminAuth.controllers");
+//const { verifyJWT } = require("../middlewares/adminAuth.validations");
+const { deleteAlumno, findAllAlumno } = require("../controllers/alumno.controller");
+
+const alumnoRouter = Router();
+
+/* Todas las rutas deben estar verificadas con JWT por que SÓLAMENTE el superAdmin debe acceder al CRUD de Administradores */
+
+/* --> Creación <-- */
+
+alumnoRouter.post("/create",
+  [
+    body("nombre").notEmpty().isString().isLength({ min: 3, max: 25} ).withMessage("Debe enviar un nombre válido."),
+    body("apellido").notEmpty().isString().isLength({ min: 3, max: 25} ).withMessage("Debe enviar un apellido válido."),
+    body("dni").notEmpty().isString().isLength({ min: 7, max: 8} ).withMessage("Debe enviar un dni válido."),
+    body("alDia").notEmpty().isBoolean().withMessage("Debe enviar si esta ala día."),
+    body("anioCursado").notEmpty().isNumeric().isLength({ min: 1, max: 1} ).withMessage("Debe enviar el año de cursado."),
+    // Faltaria un custom para verificar si el mail es único (lo hace el models pero se puede hacer aquí con .custom)
+  ],
+  verifyJWT,
+  expressValidations, 
+  dniValidation,
+  createAlumno,
+);
+
+/* --> Borrado <-- */
+
+// Operación de Borrado  por ID
+alumnoRouter.delete("/delete-by-id/:id", 
+  [
+    param("id").isMongoId().withMessage("Debe mandar un ID valido"),
+  ], 
+  verifyJWT,
+  expressValidations,
+  deleteAlumno
+);
+
+
+/* --> Busqueda <-- */
+
+// Operación de Busqueda Total
+alumnoRouter.get("/findall", findAllAlumno);
+
+// Operación de Busqueda por ID
+alumnoRouter.get("/find-by-id/:id", 
+  [
+    param("id").isMongoId().withMessage("Debe mandar un ID valido"),
+  ], 
+  verifyJWT,
+  expressValidations,
+  findAlumnoById,
+);
+
+/* --> Actualización <-- */
+
+
+
+
+
+
+module.exports = alumnoRouter;
